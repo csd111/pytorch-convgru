2fdc9ca236d359092865116dc998ba0d60179f29
csd111 <clement.doire@gmail.com>
2020-11-16 10:26:24 +0100

    Update convgru 1d cell with torchscript optimizations

--- tests/test_convgru1d.py
+++ tests/test_convgru1d.py
@@ -1,10 +1,11 @@
 import unittest
 import torch
 from convgru import ConvGRU1DCell
+from rnn_wrapper import RNN
+import time
 
 
 class ConvGRU1DTest(unittest.TestCase):
-
     def test_convgru1d_cell(self):
         # ----------------------------------------------------------------------
         # Data preparation
@@ -13,33 +14,36 @@ class ConvGRU1DTest(unittest.TestCase):
         kernel_size = 3
         padding = 1
         stride = 1
-        data =  1 + torch.randn(10, channels, 128)
+        data = 1 + torch.randn(10, channels, 128)
         hidden_state = 2 + torch.randn(10, channels, 128)
         dirac_weight = torch.zeros(channels, channels, kernel_size)
         torch.nn.init.dirac_(dirac_weight)
         dirac_weight = dirac_weight.repeat(3, 1, 1)
         # ----------------------------------------------------------------------
-        # Initialize cell to get output=tanh(input) and run it 
+        # Initialize cell to get output=tanh(input) and run it
         # ----------------------------------------------------------------------
-        cell = ConvGRU1DCell(channels, channels, kernel_size, 
-                             stride=stride, padding=padding)
+        cell = ConvGRU1DCell(
+            channels, channels, kernel_size, stride=stride, padding=padding
+        )
         # Init weights to force z=0 and n=tanh(input)
-        cell.conv_ih.weight.data = dirac_weight
-        torch.nn.init.constant_(cell.conv_hh.weight, -10**5)
+        cell.weight_ih.data = dirac_weight
+        torch.nn.init.constant_(cell.weight_hh, -(10 ** 5))
         # Run forward pass
         output_data = cell(data, hidden_state)
         self.assertLessEqual(
-            torch.max((torch.tanh(data) - output_data).abs()), 10**(-12))
+            torch.max((torch.tanh(data) - output_data).abs()), 10 ** (-12)
+        )
         # ----------------------------------------------------------------------
-        # Initialize cell to get output=hidden and run it 
+        # Initialize cell to get output=hidden and run it
         # ----------------------------------------------------------------------
         # Init weights to force z=1
-        torch.nn.init.constant_(cell.conv_ih.weight, 10**5)
-        torch.nn.init.constant_(cell.conv_hh.weight, 1)
-        # Run forward pass
+        torch.nn.init.constant_(cell.weight_ih, 10 ** 5)
+        torch.nn.init.constant_(cell.weight_hh, 1)
+        #  Run forward pass
         output_data = cell(data, hx=hidden_state)
         self.assertLessEqual(
-            torch.max((hidden_state - output_data).abs()), 10**(-12))
+            torch.max((hidden_state - output_data).abs()), 10 ** (-12)
+        )
         # ----------------------------------------------------------------------
         # Call reset_parameters() and check output != input
         # ----------------------------------------------------------------------
@@ -58,17 +62,18 @@ class ConvGRU1DTest(unittest.TestCase):
         time_steps = 64
         batch_size = 10
         nb_features = 128
-        data =  1 + torch.randn(time_steps, batch_size, channels, nb_features)
+        data = 1 + torch.randn(time_steps, batch_size, channels, nb_features)
         target = torch.tanh(data)
         # ----------------------------------------------------------------------
         # Instantiate model for training
         # ----------------------------------------------------------------------
-        cgru1d = ConvGRU1DCell(channels, channels, kernel_size, 
-                           stride=stride, padding=padding)
+        cgru1d = ConvGRU1DCell(
+            channels, channels, kernel_size, stride=stride, padding=padding
+        )
         optimizer = torch.optim.RMSprop(cgru1d.parameters(), 0.005)
         loss = torch.nn.MSELoss()
-        weights_ih_bf_train = cgru1d.conv_ih.weight.data.clone()
-        weights_hh_bf_train = cgru1d.conv_hh.weight.data.clone()
+        weights_ih_bf_train = cgru1d.weight_ih.data.clone()
+        weights_hh_bf_train = cgru1d.weight_hh.data.clone()
         # ----------------------------------------------------------------------
         # Train the model and check
         # ----------------------------------------------------------------------
@@ -77,7 +82,7 @@ class ConvGRU1DTest(unittest.TestCase):
         data = data.to(device)
         target = target.to(device)
         # Run a basic training procedure
-        for index in range(1000):
+        for index in range(500):
             # Reset the gradients
             optimizer.zero_grad()
             # Loop over the time steps
@@ -93,16 +98,21 @@ class ConvGRU1DTest(unittest.TestCase):
             error.backward()
             optimizer.step()
             if index % 100 == 0:
-                print("Step {0} / 1000 - MSError is {1}".format(index, 
-                                                                error.item()))
+                print(
+                    "Step {0} / 500 - MSError is {1}".format(
+                        index, error.item()
+                    )
+                )
         # Make sure the weights have changed
-        weights_ih_af_train = cgru1d.conv_ih.weight.data.cpu().clone()
-        weights_hh_af_train = cgru1d.conv_hh.weight.data.cpu().clone()
+        weights_ih_af_train = cgru1d.weight_ih.data.cpu().clone()
+        weights_hh_af_train = cgru1d.weight_hh.data.cpu().clone()
         self.assertIs(
-            bool(torch.all(torch.eq(weights_ih_bf_train, weights_ih_af_train))), 
-            False)
+            bool(torch.all(torch.eq(weights_ih_bf_train, weights_ih_af_train))),
+            False,
+        )
         self.assertIs(
-            bool(torch.all(torch.eq(weights_hh_bf_train, weights_hh_af_train))), 
-            False)
+            bool(torch.all(torch.eq(weights_hh_bf_train, weights_hh_af_train))),
+            False,
+        )
         # Check the final error is low enough
-        self.assertLessEqual(error.item(), 2*10**(-3))
+        self.assertLessEqual(error.item(), 5 * 10 ** (-3))
